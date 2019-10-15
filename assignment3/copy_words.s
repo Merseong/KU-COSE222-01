0a9b90abe25d6dc8b16e71d007cb78cc65271555
Merseong <esc990720@korea.ac.kr>
2019-10-15 21:30:53 +0900

    done assignment3

---
+++ assignment3/copy_words.s
@@ -0,0 +1,33 @@
+#
+#  Author: Taeweon Suh
+#  Date: Sep. 18, 2009
+#  Description: Simple addition of 4 words (result = op1 + op2)
+#
+
+.text
+.align 4
+
+main:
+	la   $t0, op1
+	la   $t1, op2
+	la   $t2, result
+	subu $t3, $t1, $t0 
+	sra  $t3, $t3, 2   # how many words? shift right by 2
+
+myloop:
+	lw   $s0, 0($t0)
+	sw   $s0, 0($t2)
+	subu $t3, $t3, 1   # decrement by 1
+	beq  $t3, $zero, myself
+	addu $t0, $t0, 4
+	addu $t2, $t2, 4
+	j    myloop
+
+myself:
+	j    myself
+
+.data
+.align 4
+op1:    .word  1, 2, 3, 4
+op2:    .word  5, 6, 7, 8
+result: .word  0, 0, 0, 0
